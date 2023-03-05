7da908ccd03a92aa7ab0ad739cbc5dc0f6c14fb7
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-03-05 18:04:26 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gbhw8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
